6940ab03980e2529798a3dafb60bc40eff056428
vdloc <uvcudlco@gmail.com>
2020-06-20 15:59:21 +0700

    add pairwise challenge

---
+++ Algorithms/pair_wise.js
@@ -0,0 +1,27 @@
+function pairwise(arr, arg) {
+  let indicesSum = 0;
+  const indices = [];
+
+  arr.forEach((item, index) => {
+    const otherOfPair = arg - item;
+    const otherItemIndex = arr.findIndex(
+      (item, idx) => item === otherOfPair && !indices.includes(idx)
+    );
+
+    if (
+      otherItemIndex > -1 &&
+      !indices.includes(index) &&
+      !indices.includes(otherItemIndex)
+    ) {
+      if (otherItemIndex === index) return;
+
+      indices.push(index, otherItemIndex);
+      indicesSum += index + otherItemIndex;
+    }
+  });
+
+  return indicesSum;
+}
+
+const pairWise = pairwise([0, 0, 0, 0, 1, 1], 1);
+console.log("pairWise", pairWise);
