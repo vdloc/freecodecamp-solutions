66f28bac56fe769c6c2b3b8e1ee645bc4205a687
vdloc <uvcudlco@gmail.com>
2025-04-24 21:39:12 +0700

    feat: add header section with title and description to the choropleth map

--- packages/data-visualization/choropleth-map/src/main.ts
+++ packages/data-visualization/choropleth-map/src/main.ts
@@ -1,33 +1,60 @@
-// import Chart from './chart';
-// import './style.css';
-
-import { centroid, geo, plot } from '@observablehq/plot';
-import { json } from 'd3';
+import './style.css';
+import {
+  geoIdentity,
+  geoPath,
+  json,
+  scaleQuantize,
+  scaleThreshold,
+  schemeBlues,
+  select,
+} from 'd3';
 import * as topojson from 'topojson-client';
 
-// const chart = new Chart({
-//   title: 'United States Educational Attainment',
-//   description:
-//     "Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)",
-//   margin: { top: 150, right: 20, bottom: 200, left: 60 },
-//   width: 1200,
-//   height: 1110,
-//   chartElement: document.getElementById('chart') as HTMLElement,
-//   tooltipElement: document.getElementById('tooltip') as HTMLElement,
-// });
-
-// chart.init();
-
+const CANVAS = {
+  w: 700,
+  h: 700,
+};
 const unemployment = await json(
   'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
 );
-const counties = await json(
+const counties: any = await json(
   'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
 );
 console.log(' unemployment:', counties);
 
-let states = topojson.feature(counties, counties.objects.states);
+const geoData: Record<string, any> = {};
+const keys = Object.keys(counties.objects);
 
+keys.forEach((key) => {
+  geoData[key] = topojson.feature(counties, counties.objects[key]);
+});
+console.log(' keys:', keys);
+const projection = geoIdentity().fitSize([CANVAS.w, CANVAS.h], geoData.nation);
+const paths = geoPath(projection);
+
+const colorScale = scaleQuantize().domain([0, 100]).range(schemeBlues[6].map((d) => parseFloat(d)));
+
+const svgContainer = select('#chart')
+  .append('svg')
+  .attr('viewBox', `0 0 ${CANVAS.w} ${CANVAS.h}`)
+  .attr('width', CANVAS.w)
+  .classed('floormap', true);
+
+const groups = svgContainer
+  .selectAll('g')
+  .data(keys)
+  .enter()
+  .append('g')
+  .attr('class', (d) => d);
+
+// Create all paths for the separate g element
+const assets = groups
+  .selectAll('path')
+  .data((d) => geoData[d]?.features)
+  .enter()
+  .append('path')
+  .attr('d', paths)
+  .attr('class', 'county');
 // plot({
 //   width: 975,
 //   height: 610,
