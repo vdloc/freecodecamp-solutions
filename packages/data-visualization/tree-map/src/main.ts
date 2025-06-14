8a4f8e5e67d0878244471b2d93a731161b40f923
vdloc <uvcudlco@gmail.com>
2025-06-14 22:45:14 +0700

    feat: refactor chart initialization and data handling for tree map visualization

--- packages/data-visualization/tree-map/src/main.ts
+++ packages/data-visualization/tree-map/src/main.ts
@@ -4,205 +4,168 @@ import {
   extent,
   geoIdentity,
   geoPath,
+  hierarchy,
+  json,
   scaleLinear,
+  scaleOrdinal,
+  ScaleOrdinal,
   ScaleQuantize,
   scaleQuantize,
   schemeBlues,
   select,
   Selection,
+  treemap,
   ValueFn,
 } from 'd3';
-import * as topojson from 'topojson-client';
-import { Topology } from 'topojson-specification';
 
-type CountyEducation = {
-  fips: number;
-  state: string;
-  area_name: string;
-  bachelorsOrHigher: number;
-};
-
-type CountyGeometry = {
-  id: number;
-  type: 'Feature';
-  properties: {
-    name: string;
-    id: number;
-    state: string;
-    [key: string]: any;
-  };
-  geometry: {
-    type: 'Polygon' | 'MultiPolygon';
-    coordinates: [number, number][][];
-  };
-};
+type Data = { children: Data[]; name: string; value?: string | number } | null;
 
 class Chart {
   private static readonly DATA_URL =
     'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json';
-  private static readonly COUNTIES_DATA_URL =
-    'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
-  private unemployment: CountyEducation[] = [];
-  private counties: Topology | null = null;
-  private geoData: Record<string, any> = {};
+  private data: Data = null;
 
-  public width: number;
-  public height: number;
+  public static readonly WIDTH = 900;
+  public static readonly HEIGHT = 600;
+  private legendWidth = 600;
+  private legendHeight = 400;
+
   private chartElement: HTMLElement;
   private toolTipElement: HTMLElement;
   private svg: Selection<SVGSVGElement, unknown, null, any> | null;
-  private colorScale: ScaleQuantize<string, string> | null;
-  private paths: ValueFn<SVGPathElement, unknown, any> | null;
+  private colorScale: ScaleOrdinal<string, string> | null;
 
   constructor() {
-    this.width = 900;
-    this.height = 900;
     this.chartElement = document.getElementById('chart') as HTMLElement;
     this.toolTipElement = this.chartElement.querySelector(
       '#tooltip'
     ) as HTMLElement;
     this.svg = null;
     this.colorScale = null;
-    this.paths = null;
+    this.data = null;
     this.init();
   }
 
   async init() {
-    let data = await this.getData();
-    console.log(' data:', data);
-
-    this.geoData = this.getGeoData();
-    this.colorScale = this.getColorScale();
-    this.paths = this.getPaths();
     this.svg = select(this.chartElement)
       .append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height);
-    this.createPlots();
+      .attr('width', Chart.WIDTH)
+      .attr('height', Chart.HEIGHT);
+    this.data = (await json(Chart.DATA_URL)) as Data;
+    this.colorScale = this.getColorScale();
     this.createLegend();
-  }
-
-  async getData() {
-    return await fetch(Chart.DATA_URL).then((res) => res.json());
-  }
-
-  getGeoData() {
-    if (!this.counties) return {};
-
-    const { objects } = this.counties as Topology;
-    const geoData: Record<keyof typeof objects, any> = {};
-
-    for (let key in objects) {
-      geoData[key as keyof typeof objects] = topojson.feature(
-        this.counties,
-        objects[key]
-      );
-    }
-
-    return geoData;
-  }
-
-  getPaths(): ValueFn<SVGPathElement, unknown, any> {
-    const projection = geoIdentity().fitSize(
-      [this.width, this.height],
-      this.geoData.nation
-    );
-    return geoPath(projection) as ValueFn<SVGPathElement, unknown, any>;
+    this.createPlots();
   }
 
   getColorScale() {
-    let dataRange = extent(
-      this.unemployment.map((record) => Number(record.bachelorsOrHigher))
+    let children = this.data?.children ?? [];
+    let devices = children.map((child) => child?.name ?? '');
+    let colors = Array.from({ length: devices.length }, () =>
+      this.getRandomColor()
     );
 
-    return scaleQuantize(dataRange as number[], schemeBlues[7]);
+    return scaleOrdinal(devices as string[], colors);
   }
 
   createPlots() {
     if (!this.svg) return;
+    const root = hierarchy(this.data)
+      .sum((d) => Number(d?.value) || 0)
+      .sort((a, b) => (b?.value ?? 0) - (a?.value ?? 0));
+    const treeMap = treemap<unknown>()
+      .size([Chart.WIDTH, Chart.HEIGHT - this.legendHeight])
+      .padding(1);
 
-    this.svg
-      .selectAll('path')
-      .data(this.geoData.counties.features)
+    treeMap(root as any);
+    const groups = this.svg
+      .selectAll('g')
+      .data(root.leaves() as any[])
       .enter()
-      .append('path')
-      .attr('d', this.paths)
-      .attr('class', 'county')
-      .attr('fill', (datum) => {
-        const { id } = datum as CountyGeometry;
-        const percent = this.unemployment.find((item) => item.fips === id);
-
-        return this.colorScale
-          ? this.colorScale(Number(percent?.bachelorsOrHigher) ?? 4)
-          : null;
-      })
-      .attr('data-fips', (datum) => {
-        const { id } = datum as CountyGeometry;
-        const percent = this.unemployment.find((item) => item.fips === id);
-
-        return percent?.fips || id;
-      })
-      .attr('data-education', (datum) => {
-        const { id } = datum as CountyGeometry;
-        const percent = this.unemployment.find((item) => item.fips === id);
-
-        return percent?.bachelorsOrHigher ?? 4;
-      })
-      .on('mouseover', (event, datum) => {
-        const { id } = datum as CountyGeometry;
-        const percent = this.unemployment.find((item) => item.fips === id);
-        const tooltip = select(this.toolTipElement);
-
-        tooltip
-          .style('opacity', 1)
-          .style('visibility', 'visible')
-          .attr('id', 'tooltip')
-          .attr('data-education', percent?.bachelorsOrHigher ?? 4)
-          .html(
-            `${percent?.area_name}, ${percent?.state}: ${percent?.bachelorsOrHigher}%`
-          )
-          .style('left', `${event.pageX}px`)
-          .style('top', `${event.pageY}px`);
-      })
-      .on('mouseout', () => {
-        const tooltip = select(this.toolTipElement);
-        tooltip.style('opacity', 0).style('visibility', 'hidden');
+      .append('g')
+      .attr('transform', (d: any) => {
+        console.log(' d:', d);
+        return `translate(${d.x0}, ${d.y0})`;
       });
+
+    groups
+      .append('rect')
+      .attr('width', (d) => d.x1 - d.x0)
+      .attr('height', (d) => d.y1 - d.y0)
+      .attr('fill', (d) => {
+        return this.colorScale ? this.colorScale(d.data.name) : '';
+      });
+
+    groups
+      .append('text')
+      .text((d) => d.data.name)
+      .attr('x', 5)
+      .attr('y', 15)
+      .attr('fill', 'white')
+      .attr('font-size', '8px')
+      .attr('pointer-events', 'none');
   }
 
   createLegend() {
     if (!this.svg) return;
 
-    const percentRange = Array.from({ length: 8 }, (_, index) => 3 + index * 9);
-    const domainRange = Array.from({ length: 8 }, (_, index) => index * 50);
-    const legendWidth = 400;
-    const legend = this.svg
-      .append('g')
-      .attr('id', 'legend')
-      .attr('transform', `translate(${this.width - legendWidth}, 100)`);
-
-    const legendScale = scaleLinear(percentRange, domainRange);
-    const ticksDistance =
-      legendScale(percentRange[1]) - legendScale(percentRange[0]);
-    const legendAxis = axisBottom(legendScale)
-      .tickValues(percentRange)
-      .tickSize(15)
-      .tickFormat((d) => `${d}%`);
+    const legend = this.svg.append('g').attr('id', 'legend');
     legend
-      .selectAll('rect')
-      .data(percentRange.slice(0, -1))
-      .enter()
-      .append('rect')
-      .attr('x', (d) => legendScale(d))
-      .attr('y', -0)
-      .attr('width', ticksDistance)
-      .attr('height', 15)
-      .attr('style', 'position: relative;z-index: 99')
-      .attr('fill', (d) => (this.colorScale ? this.colorScale(d) : 'black'))
-      .attr('class', 'legend-item');
-    legend.append('g').attr('class', 'axis').call(legendAxis);
+      .attr(
+        'transform',
+        `translate(${(Chart.WIDTH - this.legendWidth) / 2}, ${
+          Chart.HEIGHT - this.legendHeight
+        })`
+      )
+      .attr('width', this.legendWidth)
+      .attr('height', this.legendHeight);
 
-    legend.select('.domain').remove();
+    const legendItem = legend
+      .selectAll('g')
+      .data(this.data?.children.map((child) => child?.name) || [])
+      .join('g')
+      .attr('width', this.legendWidth / 3)
+      .attr('height', 20)
+      .attr(
+        'transform',
+        (d, i) =>
+          `translate(${(i % 3) * (this.legendWidth / 3)}, ${
+            Math.floor(i / 3) * (20 + 10)
+          })`
+      );
+
+    legendItem
+      .append('rect')
+      .attr('width', 20)
+      .attr('height', 20)
+      .attr('fill', (d) => {
+        return this.colorScale ? this.colorScale(d as string) : '';
+      });
+    legendItem
+      .append('text')
+      .text((d) => d as string)
+      .attr('x', 25)
+      .attr('y', 15);
+
+    const bbox = legend.node()?.getBBox();
+
+    if (bbox) {
+      this.legendWidth = bbox.width;
+      this.legendHeight = bbox.height;
+      legend.attr(
+        'transform',
+        `translate(${(Chart.WIDTH - this.legendWidth) / 2}, ${
+          Chart.HEIGHT - this.legendHeight
+        })`
+      );
+    }
+  }
+
+  getRandomColor() {
+    return Array.from({ length: 3 }, () =>
+      Math.floor(Math.random() * 256)
+    ).reduce((colorString, part, index) => {
+      return `${colorString}${part}${index < 2 ? ',' : ')'}`;
+    }, 'rgb(');
   }
 }
 
